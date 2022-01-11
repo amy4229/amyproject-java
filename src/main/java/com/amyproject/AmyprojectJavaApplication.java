ce928f5b6ba61f0b8c57cf8d7acc80bb499d9a16
amy <amykim4229@gmail.com>
2022-01-11 16:52:23 +0900

    initialize javaproject
    2022-01-11 Amy 프로젝트 셋업

---
+++ src/main/java/com/amyproject/AmyprojectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.amyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmyprojectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmyprojectJavaApplication.class, args);
+	}
+
+}
